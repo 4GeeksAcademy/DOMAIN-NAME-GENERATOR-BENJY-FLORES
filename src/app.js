46189f8ef26a4a84a3e6f574a6067985883a909c
Benjyflores <113569896+Benjyflores@users.noreply.github.com>
2023-11-22 20:28:24 +0000

    casi termnado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "a", "some", "another", "any"];
+  let adjective = ["skinny", "tall", "pretty", "ugly", "funny"];
+  let noun = ["doggo", "hamster", "guy", "cat"];
+  let tdl = [".com", ".org", ".us", ".io", ".net"];
+
+  let domainNames = [];
+
+  for (let pronoun of pronoun) {
+    for (let adjective of adjective) {
+      for (let noun of noun) {
+        for (let tdl of tdl) {
+          domainNames.push(`${pronoun}${adjective}${noun}${tdl}`);
+        }
+      }
+    }
+  }
 };
